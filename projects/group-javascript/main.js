799e0b672804281b46040a7f867f06b2b3f02c8d
dalag884 <dalag884@newschool.edu>
2017-03-29 21:39:41 -0400

    added group java

---
+++ projects/group-javascript/main.js
@@ -0,0 +1,34 @@
+
+console.log ('hello world');
+
+var springBreak = ['suprised my family in India'];
+
+console.log(6 * 11)
+console.log(33 + 33)
+console.log(67 - 1)
+console.log(6 * (5 + 6))
+console.log(132/2)
+console.log((2 * 3) * (5 + 6));
+
+var groupOne= ['Ana', 'Gaurika', 'Moon'];
+var groupTwo= ['Vitoria', 'Liz', 'Jissaura']
+var groupThree = ['Kyle', 'Carol', 'Fish'];
+
+var allStudentsHere = false;
+
+var studentsMissing = true;
+
+var arrayOfArrays =[[
+  'Carol Chong',
+  'Gaurika Milan Dalal',
+  'Vitoria Feitosa Di Iulio',
+  'Nafisa Afrin Iqbal',
+  'Moonnyung Jo',
+  'Ana Sofia Murillo',
+  'Jissaura Taveras Hernandez',
+  'Elizabeth Tsai',
+  'Kyle Yu',
+],
+['Ana', 'Gaurika', 'Moon'],
+['Victoria', 'Liz', 'Jissaura'],
+['Kyle', 'Carol', 'Fish']];
