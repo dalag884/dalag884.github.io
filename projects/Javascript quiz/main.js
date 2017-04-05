a2b4e0f5240c92a76fcc1961b11a64b8b5beb0f3
dalag884 <dalag884@newschool.edu>
2017-04-05 19:46:23 -0400

    javascript quiz

---
+++ projects/Javascript quiz/main.js
@@ -0,0 +1,41 @@
+console.log ('hello world');
+
+var myName = ['gaurika'];
+console.log(myName);
+
+var favoriteInteger = [9];
+console.log(favoriteInteger);
+
+var favoriteNumber = ['9'];
+console.log(favoriteNumber);
+
+var allStudentsHere = true;
+console.log(allStudentsHere);
+
+var myArray = [ 'g','a','u','r','i','k','a'];
+console.log(myArray);
+
+var randomArray = ['hello', 'world', 1, 2, true, false, 'lorem ipsum']
+console.log(randomArray);
+
+var myName = ['gaurika', 'dalal'];
+console.log('gaurika', 'dalal')
+
+var mathProblem = [50 + 50];
+console.log(50 + 50);
+
+
+var firstItem = myArray[0];
+var lastItem = myArray[6];
+
+var longString = ['s','u','p','e','r','c','a','l','i','f','r','a','g','i','l','i','s','t','i','c','e','x','p','i','a','l','i','d','o','c','i','o','u','s'];
+console.log(longString) = length;
+
+var allStudentsHere = true;
+
+if ( allStudentsHere== true ) {
+  console.log("all Students are in class");
+} else {
+  console.log("all students not in class");
+}
+
