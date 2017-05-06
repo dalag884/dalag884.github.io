6c8816a68f652a90e119b617d9c798811f7f3226
dalag884 <dalag884@newschool.edu>
2017-05-06 19:24:32 -0400

    time dream vacay coding

--- projects/dreamvacay-coding/main.js
+++ projects/dreamvacay-coding/main.js
@@ -1,3 +1,18 @@
+function updateTime() {
+  var date = new Date();
+  $('#time').html( date.toGMTString() );
+}
+
 $(function() {
-  // Your interactions go here
+  updateTime();
+  setInterval(updateTime, 1000);
 });
+
+function updateTime() {
+  var localDate = new Date();
+  var utc = localDate.getTime() + ( localDate.getTimezoneOffset() * 60000 );
+  var indiaUtcOffset = 5.5;
+  var indiaDate = new Date( utc + ( 3600000 * indiaUtcOffset ) );
+  var ist = indiaDate.toLocaleString();
+  $('#time2').html( ist );
+}
