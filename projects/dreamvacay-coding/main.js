243d6ebaa1b70911c2315954b45a6edb06891daa
dalag884 <dalag884@newschool.edu>
2017-05-08 18:01:09 -0400

    changes in dream vacay coding

--- projects/dreamvacay-coding/main.js
+++ projects/dreamvacay-coding/main.js
@@ -1,14 +1,8 @@
-function updateTime() {
-  var date = new Date();
-  $('#time').html( date.toGMTString() );
-}
-
-$(function() {
-  updateTime();
-  setInterval(updateTime, 1000);
-});
 
 function updateTime() {
+    var date = new Date();
+    $('#time').html( date.toLocaleString() );
+
   var localDate = new Date();
   var utc = localDate.getTime() + ( localDate.getTimezoneOffset() * 60000 );
   var indiaUtcOffset = 5.5;
@@ -16,3 +10,29 @@ function updateTime() {
   var ist = indiaDate.toLocaleString();
   $('#time2').html( ist );
 }
+
+$(function() {
+  updateTime();
+  setInterval(updateTime, 1000);
+});
+
+
+
+$(function() {
+  $('.image').mouseenter(function(event) {
+    // console.log('event.pageX is', event.pageX);
+    // console.log('event.pageY is', event.pageY);
+
+    var id = $(this).attr('data-image-id');
+    $('[data-hover-image-id="' + id + '"]')
+      .addClass('active')
+      .css({
+        left: event.pageX + 'px',
+        top: event.pageY + 'px'
+      });
+  });
+
+  $('.image').mouseleave(function() {
+    $('.hover-images img').removeClass('active');
+  });
+});
