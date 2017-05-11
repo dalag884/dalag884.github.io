0a753d4ba0bbb7af5b3dda2d495dc4d123bef818
dalag884 <dalag884@newschool.edu>
2017-05-11 15:03:46 -0400

    changed to dream vacay

--- projects/dreamvacay-coding/main.js
+++ projects/dreamvacay-coding/main.js
@@ -8,12 +8,25 @@ function updateTime() {
   var indiaUtcOffset = 5.5;
   var indiaDate = new Date( utc + ( 3600000 * indiaUtcOffset ) );
   var ist = indiaDate.toLocaleString();
+
   $('#time2').html( ist );
+
+  var hour = date.getHours();
+
+  if ( hour > 6 && hour < 12 ){
+    console.log('breakfast');
+    $('.breakfast').css( 'display', 'inline');
+  } else if ( hour >= 12 && hour < 19 ) {
+    console.log('lunch');
+    $('.lunch').css( 'display', 'inline');
+  } else {
+    console.log('dinner');
+    $('.dinner').css( 'display', 'inline');
+  }
 }
 
 $(function() {
   updateTime();
   setInterval(updateTime, 1000);
 });
-
 
